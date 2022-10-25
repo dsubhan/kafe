65a0b376282f4e2662d384a6d88d2b97fe9316d4
David Subhan <83970157+dsubhan@users.noreply.github.com>
2022-10-25 14:10:40 +0200

    Update db.php
    Hotová aplikace

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$server = 'db.dw029.webglobe.com';
-$user = 'david_subhan_cz';
+$server = '<db server>';
+$user = '<user>';
 $pass = '****';
-$dbname = 'david_subhan_cz';
+$dbname = '<db name>';
 $con = mysqli_connect($server, $user, $pass) or die("Can't connect");
 mysqli_select_db($con, $dbname);
 ?>
