2bf01e52e6c1751737dda495ca6f2a3dfebf29f8
dsubhan <davidsubhan04@gmail.com>
2022-10-25 14:09:09 +0200

    hotová aplikace

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $server = 'db.dw029.webglobe.com';
 $user = 'david_subhan_cz';
-$pass = 'v6Hw9FXq';
+$pass = '****';
 $dbname = 'david_subhan_cz';
 $con = mysqli_connect($server, $user, $pass) or die("Can't connect");
 mysqli_select_db($con, $dbname);
